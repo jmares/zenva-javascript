449d30aa2775c97ac8a9fabd86a6dd8c1052f0f2
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-12-04 13:17:52 +0100

    Zenva Nodejs Express Beginners, lessons 6 .. 9

--- 10-nodejs-express-beginners/rest-api/index.js
+++ 10-nodejs-express-beginners/rest-api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/', (request, response) => {
+    response.send('Hello world');
+});
+
+app.get('/status', (request, response) => {
+    response.status(200).json({ message: 'ok', status: 200});
+});
+
+app.post('/signup', (request, response) => {
+    response.status(200).json({ message: 'ok', status: 200});
+});
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 });
