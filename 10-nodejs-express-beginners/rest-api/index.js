c8ee4dedfb1ec3d0a3356b587855044c9dd72d82
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-12-05 14:53:57 +0100

    Zenva Nodejs Express Beginners, lesson 15

--- 10-nodejs-express-beginners/rest-api/index.js
+++ 10-nodejs-express-beginners/rest-api/index.js
@@ -3,15 +3,17 @@ require('dotenv').config();
 //const { response } = require('express');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 3000; 
-
+const cors = require('cors');
 const routes = require('./routes/main');
 const passwordRoutes = require('./routes/password');
+
+const app = express();
+const port = process.env.PORT || 3000; 
 
 // updating express settings
 app.use(bodyParser.urlencoded({ extended: false})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN }))
 
 // setup routes
 
