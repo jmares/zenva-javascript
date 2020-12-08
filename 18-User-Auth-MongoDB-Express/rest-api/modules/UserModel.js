f7dd81fe5606b8c002b57fbe24398931a7e0d69d
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-12-08 15:10:10 +0100

    Zenva User Authentication with MongoDB and Express, lessons 6

---
+++ 18-User-Auth-MongoDB-Express/rest-api/modules/UserModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    resetToken: {
+        type: String,
+    },
+    resetTokenExp: {
+        type: Date
+    }
+});
+
+UserSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
+
+UserSchema.methods.isValidPassword = async function(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+};
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
